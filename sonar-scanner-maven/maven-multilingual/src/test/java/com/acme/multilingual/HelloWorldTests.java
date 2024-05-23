f11fc07bb90ece601e6cfd1c68104f72d2fc156e
Bharat Prakash Parakh <bharatparakh14@gmail.com>
2024-05-23 19:12:31 +0530

    Update and rename HelloWorldTest.java to HelloWorldTests.java

--- sonar-scanner-maven/maven-multilingual/src/test/java/com/acme/multilingual/HelloWorldTests.java
+++ sonar-scanner-maven/maven-multilingual/src/test/java/com/acme/multilingual/HelloWorldTests.java
@@ -2,10 +2,10 @@ package com.acme.multilingual;
 
 import org.junit.Test;
 
-public class HelloWorldTest {
+public class HelloWorldTests {
 
   @Test
   public void sayHello() {
-    new HelloWorld().sayHello();
+    new HelloWorld().sayHellos();
   }
 }
